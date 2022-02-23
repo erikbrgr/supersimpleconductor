117ab20b79e4d11e2f61e2ea5364f3fa88f7c01f
Evan Moore <evan@themoores.ca>
2022-02-23 12:52:29 -0500

    Ingore Null vlues in ConductorTaskResult

--- ConductorClient/Models/ConductorTaskResult.cs
+++ ConductorClient/Models/ConductorTaskResult.cs
@@ -18,15 +18,15 @@ namespace SuperSimpleConductor.ConductorClient.Models
       public Guid WorkflowInstanceId { get; set; }
       [JsonProperty("taskId")]
       public Guid TaskId { get; set; }
-      [JsonProperty("workerId")]
+      [JsonProperty("workerId", NullValueHandling = NullValueHandling.Ignore)]
       public Guid? WorkerId { get; set; }
       [JsonProperty("status")]
       public string Status { get; set; }
-      [JsonProperty("reasonForIncompletion")]
+      [JsonProperty("reasonForIncompletion", NullValueHandling = NullValueHandling.Ignore)]
       public string ReasonForIncompletion { get; set; }
-      [JsonProperty("outputData")]
+      [JsonProperty("outputData", NullValueHandling = NullValueHandling.Ignore)]
       public ConductorTaskData OutputData { get; set; }
-      [JsonProperty("logs")]
+      [JsonProperty("logs", NullValueHandling = NullValueHandling.Ignore)]
       public ICollection<ConductorTaskLog> Logs { get; set; }
       [JsonProperty("callbackAfterSeconds")]
       public long CallbackAfterSeconds { get; set; }
