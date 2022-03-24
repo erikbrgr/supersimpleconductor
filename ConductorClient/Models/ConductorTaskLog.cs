bae6a00663173121dd21f5fc923882f1a0a49980
Erik Burger <erik.burger@solera.nl>
2022-03-24 11:08:03 +0100

    Added JsonProperty to all Models. Bumped version

--- ConductorClient/Models/ConductorTaskLog.cs
+++ ConductorClient/Models/ConductorTaskLog.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace SuperSimpleConductor.ConductorClient.Models
 {
    public class ConductorTaskLog
    {
+      [JsonProperty("log")] 
       public string Log { get; set; }
+      [JsonProperty("taskId")] 
       public Guid TaskId { get; set; }
+      [JsonProperty("createdTime")] 
       public long CreatedTime { get; set; }
    }
 }
--- ConductorClient/Models/ConductorWorkflowTask.cs
+++ ConductorClient/Models/ConductorWorkflowTask.cs
@@ -1,14 +1,24 @@
-﻿namespace SuperSimpleConductor.ConductorClient.Models
+﻿using Newtonsoft.Json;
+
+namespace SuperSimpleConductor.ConductorClient.Models
 {
    public partial class ConductorWorkflowTask
    {
+      [JsonProperty("name")]
       public string Name { get; set; }
+      [JsonProperty("taskReferenceName")]
       public string TaskReferenceName { get; set; }
+      [JsonProperty("inputParameters")]
       public ConductorTaskData InputParameters { get; set; }
+      [JsonProperty("type")]
       public string Type { get; set; }
+      [JsonProperty("startDelay")]
       public long StartDelay { get; set; }
+      [JsonProperty("optional")]
       public bool Optional { get; set; }
+      [JsonProperty("taskDefinition")]
       public ConductorWorkflowTaskDefinition TaskDefinition { get; set; }
+      [JsonProperty("asyncComplete")]
       public bool AsyncComplete { get; set; }
    }
 }
