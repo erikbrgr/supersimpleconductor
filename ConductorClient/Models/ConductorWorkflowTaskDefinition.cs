1df87ccaa88a665b8c6f5bae99db18b8a796ef6f
Erik Burger <erik@mindcode.nl>
2022-03-24 11:17:04 +0100

    Case sensitive payloads 2 (#10)
    * camelCase ConductorTaskResult
    
    * Add payload logging
    
    * add vs code artifacts
    
    * add sample to TestWorker
    
    * Ingore Null vlues in ConductorTaskResult
    
    * Added JsonProperty to all Models. Bumped version
    
    Co-authored-by: Evan Moore <evan@themoores.ca>
    Co-authored-by: Erik Burger <erik.burger@solera.nl>

--- ConductorClient/Models/ConductorWorkflowTaskDefinition.cs
+++ ConductorClient/Models/ConductorWorkflowTaskDefinition.cs
@@ -1,21 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace SuperSimpleConductor.ConductorClient.Models
 {
    public partial class ConductorWorkflowTaskDefinition
    {
+      [JsonProperty("createTime")]
       public long CreateTime { get; set; }
+      [JsonProperty("name")]
       public string Name { get; set; }
+      [JsonProperty("description")]
       public string Description { get; set; }
+      [JsonProperty("retryCount")]
       public long RetryCount { get; set; }
+      [JsonProperty("timeoutSeconds")]
       public long TimeoutSeconds { get; set; }
+      [JsonProperty("inputKeys")]
       public IEnumerable<string> InputKeys { get; set; }
+      [JsonProperty("outputKeys")]
       public IEnumerable<string> OutputKeys { get; set; }
+      [JsonProperty("timeoutPolicy")]
       public string TimeoutPolicy { get; set; }
+      [JsonProperty("retryLogic")]
       public string RetryLogic { get; set; }
+      [JsonProperty("retryDelaySeconds")]
       public long RetryDelaySeconds { get; set; }
+      [JsonProperty("responseTimeoutSeconds")]
       public long ResponseTimeoutSeconds { get; set; }
+      [JsonProperty("rateLimitPerFrequency")]
       public long RateLimitPerFrequency { get; set; }
+      [JsonProperty("rateLimitFrequencyInSeconds")]
       public long RateLimitFrequencyInSeconds { get; set; }
    }
 }
